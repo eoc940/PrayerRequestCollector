15c1b60995759fd3cd844c94ef53636d684b9d4d
Hyunggeun Kim <hgk0000@naver.com>
2024-01-09 09:53:43 +0900

    feat: tabbar, 탭당 뷰 구현

---
+++ PrayerRequestCollector/PresentationLayer/MemberView.swift
@@ -0,0 +1,20 @@
+//
+//  MemberView.swift
+//  PrayerRequestCollector
+//
+//  Created by Khg Dev on 2024/01/09.
+//
+
+import SwiftUI
+
+struct MemberView: View {
+    var body: some View {
+        Text("member")
+    }
+}
+
+struct MemberView_Previews: PreviewProvider {
+    static var previews: some View {
+        MemberView()
+    }
+}
---
+++ PrayerRequestCollector/PresentationLayer/SearchView.swift
@@ -0,0 +1,20 @@
+//
+//  SearchView.swift
+//  PrayerRequestCollector
+//
+//  Created by Khg Dev on 2024/01/09.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text("search")
+    }
+}
+
+struct SearchView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchView()
+    }
+}
