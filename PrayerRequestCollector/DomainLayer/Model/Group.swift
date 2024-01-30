2fc0a8621ece03219d0e17861a9411e2481ab2ad
Hyunggeun Kim <hgk0000@naver.com>
2024-01-30 10:56:01 +0900

    feat: group createdDate 추가

--- PrayerRequestCollector/DomainLayer/Model/Group.swift
+++ PrayerRequestCollector/DomainLayer/Model/Group.swift
@@ -11,10 +11,12 @@ struct Group: Identifiable {
     let id: String = UUID().uuidString
     let name: String
     let members: [Member]
+    let createdDate: Date
     
-    init(name: String, members: [Member] = []) {
+    init(name: String, members: [Member] = [], date: Date = Date()) {
         self.name = name
         self.members = members
+        self.createdDate = date
     }
 }
 
